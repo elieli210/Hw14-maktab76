227ec60605ec99db80cc99f226e8b13b4acf501f
Elmira <elmira.rezabeyk@gmail.com>
2022-07-06 22:52:47 +0430

    ex2 done!

--- ex1/script.js
+++
@@ -1,9 +0,0 @@
-$(".container #paragraph").css({
-  "background-color": "pink",
-  color: "black",
-  margin: "10px",
-  height: "30px",
-  justifyContent: "center",
-  display: "flex",
-  flexDirection: "column",
-});
